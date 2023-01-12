b7014978b922b13d1de3b0123c100954de0bd44c
AbdassalamAhmad <abdassalam.ahmad98@gmail.com>
2023-01-12 13:07:34 +0200

    add http access of sg

--- ec2.tf
+++ ec2.tf
@@ -35,6 +35,14 @@ resource "aws_security_group" "BastionSG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "allow http from ALL"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
