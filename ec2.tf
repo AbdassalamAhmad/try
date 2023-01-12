3be3cad80fd45ca415e24b1eb75429e2c3da0e3f
AbdassalamAhmad <abdassalam.ahmad98@gmail.com>
2023-01-12 17:02:39 +0200

    remove user data

--- ec2.tf
+++ ec2.tf
@@ -11,16 +11,16 @@ resource "aws_instance" "bastion_instance_1" {
   }
 
 
-  user_data = <<-EOF
-    #!/bin/bash
-    # Use this for your user data (script from top to bottom)
-    # install httpd (Linux 2 version)
-    yum update -y
-    yum install -y httpd
-    systemctl start httpd
-    systemctl enable httpd
-    echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
-    EOF
+  # user_data = <<-EOF
+  #   #!/bin/bash
+  #   # Use this for your user data (script from top to bottom)
+  #   # install httpd (Linux 2 version)
+  #   yum update -y
+  #   yum install -y httpd
+  #   systemctl start httpd
+  #   systemctl enable httpd
+  #   echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+  #   EOF
 }
 
 resource "aws_security_group" "BastionSG" {
